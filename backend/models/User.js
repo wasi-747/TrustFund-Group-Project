7ac7f52e726680c41a97d02291ab89e0b87a541a
Wasiur Rahman Sakib <wasisakib7846@gmail.com>
2025-12-10 02:34:16 +0600

    Added Auth System and Frontend Pages

---
+++ backend/models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["admin", "fundraiser", "contributor"],
+    default: "contributor",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("User", UserSchema);
