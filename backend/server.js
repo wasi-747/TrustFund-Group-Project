8bd19b2c1ad8eaef8f380021069b47b3b912888f
Wasiur Rahman Sakib <wasisakib7846@gmail.com>
2025-12-10 01:14:08 +0600

    Initial Commit: Week 1 Skeleton

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+// Database Connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ MongoDB Connected!"))
+  .catch((err) => console.error("❌ Database Connection Error:", err));
+
+// Routes (To be added by Member 3)
+app.use("/api/auth", require("./routes/auth"));
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
